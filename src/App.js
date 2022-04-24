b4135ced614a0033637a9f6d9a2e6ddc30bc904d
Cephas-prog <cephas-gondwe@hotmail.co.uk>
2022-04-24 20:45:45 +0100

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<h1>Portfolio</h1>
+		</div>
+	);
+}
+
+export default App;
